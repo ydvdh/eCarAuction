5001313da5911018016eccd43f7554cb14c17d6e
ydvdh <yadavdhkl@gmail.com>
2023-12-28 15:15:44 -0500

    Using Zustand for state management

--- web.carauction/app/actions/auctionActions.ts
+++ web.carauction/app/actions/auctionActions.ts
@@ -2,8 +2,8 @@
 
 import { Auction, PagedResult } from "@/types";
 
-export async function getData(pageNumber: number, pageSize: number): Promise<PagedResult<Auction>> {
-    const response = await fetch(`http://localhost:6001/api/search?pageSize=${pageSize}&pageNumber=${pageNumber}`)
+export async function getData(query: string): Promise<PagedResult<Auction>> {
+    const response = await fetch(`http://localhost:6001/api/search${query}`)
     if (!response.ok) throw new Error('Failed to fetch data')
 
     return response.json();
--- web.carauction/app/auctions/Filters.tsx
+++ web.carauction/app/auctions/Filters.tsx
@@ -1,29 +1,29 @@
 import { Button } from 'flowbite-react';
 import React from 'react'
+import { useParamsStore } from '../hooks/useParamsStore';
 
-type Props = {
-    pageSize: number;
-    setPageSize: (size: number) => void;
-}
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({pageSize, setPageSize}: Props) {
-  return (
-    <div className='flex justify-between items-center mb-4'>
-        <div>
-            <span className='uppercase text-sm text-gray-500 mr-2'>Filter by</span>
-            <Button.Group>
-                    {pageSizeButtons.map((value , i) => (
-                        <Button
-                            key={i}
-                            onClick={() => setPageSize(value)}
-                            color={`${pageSize === value ? 'red' : 'gray'}`}
-                            className='focus ring-0' >
-                           {value}
-                        </Button>
-                    ))}
-            </Button.Group>
+export default function Filters() {
+    const pageSize = useParamsStore(state=>state.pageSize);
+    const setParams = useParamsStore(state=>state.setParams)
+
+    return (
+        <div className='flex justify-between items-center mb-4'>
+            <div>
+                <span className='uppercase text-sm text-gray-500 mr-2'>Filter by</span>
+                <Button.Group>
+                        {pageSizeButtons.map((value , i) => (
+                            <Button
+                                key={i}
+                                onClick={() => setParams({pageSize: value})}
+                                color={`${pageSize === value ? 'red' : 'gray'}`}
+                                className='focus ring-0' >
+                            {value}
+                            </Button>
+                        ))}
+                </Button.Group>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
