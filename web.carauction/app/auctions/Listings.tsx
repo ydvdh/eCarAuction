9af1d23a8e5a3271b7c98c5df18053fd6d0c04c6
ydvdh <yadavdhkl@gmail.com>
2023-12-28 12:51:42 -0500

    Display data with css design

--- web.carauction/app/auctions/Listings.tsx
+++ web.carauction/app/auctions/Listings.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import AuctionCard from './AuctionCard';
 
 async function getData() {
-    const response = await fetch('http://localhost:6001/api/search')
+    const response = await fetch('http://localhost:6001/api/search?pageSize=10')
     if (!response.ok) throw new Error('Failed to fetch data')
 
     return response.json();
@@ -10,6 +11,10 @@ async function getData() {
 export default async function Listings() {
     const data = await getData();
     return (
-        <div>{JSON.stringify(data, null, 2)}</div>
+        <div className='grid grid-cols-4 gap-6'>
+            {data && data.result?.map((auction: any) => (
+                <AuctionCard auction={auction} key={auction.id} />
+            ))}
+        </div>
     )
 }
