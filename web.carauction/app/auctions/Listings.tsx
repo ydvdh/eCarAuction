841fe11ee4c7d8183a2e954475aad3813c8cd664
ydvdh <yadavdhkl@gmail.com>
2023-12-29 13:10:12 -0500

    Component to show when no data present

--- web.carauction/app/auctions/Listings.tsx
+++ web.carauction/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '../hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 export default  function Listings() {
     const [data, setData] = useState<PagedResult<Auction>>()
@@ -38,14 +39,18 @@ export default  function Listings() {
     return (
         <>
             <Filters />
-            <div className='grid grid-cols-4 gap-6'>
-                { data.result.map(auction => (
-                    <AuctionCard auction={auction} key={auction.id} />
-                ))}
-            </div>
-            <div className='flex justify-center mt-5'>
-                <AppPagination currentPage={params.pageNumber} pageChanged={setPageNumber} pageCount={data.pageCount} />
-            </div>
+            {data.totalCount === 0 ? (<EmptyFilter showReset />):(
+                <>
+                    <div className='grid grid-cols-4 gap-6'>
+                        { data.result.map(auction => (
+                        <AuctionCard auction={auction} key={auction.id} />
+                        ))}
+                    </div>
+                    <div className='flex justify-center mt-5'>
+                        <AppPagination currentPage={params.pageNumber} pageChanged={setPageNumber} pageCount={data.pageCount} />
+                    </div>
+                </>
+            )}
         </>
     )
 }
