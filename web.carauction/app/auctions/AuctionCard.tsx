9b4e4ab4d755c266fe7dee94362bb74e779c2e11
ydvdh <yadavdhkl@gmail.com>
2023-12-31 17:20:21 -0500

    React form, Re-useable component for Input and DateInput,

--- web.carauction/app/auctions/AuctionCard.tsx
+++ web.carauction/app/auctions/AuctionCard.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import CountdownTimer from './CountdownTimer'
 import CarImage from './CarImage'
 import { Auction } from '@/types'
+import Link from 'next/link'
 
 type Props = {
     auction: Auction
 }
 export default function ({auction}: Props) {
   return (
-    <a href='#'>
+    <Link href={`/auctions/details/${auction.id}`}>
         <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
             <div>
                 <CarImage imageUrl= {auction.imageUrl} />
@@ -22,6 +23,6 @@ export default function ({auction}: Props) {
             <p className='font-semibold text-sm'>{auction.year}</p>
         </div>
 
-    </a>
+    </Link>
   )
 }
---
+++ web.carauction/app/auctions/AuctionForm.tsx
@@ -0,0 +1,79 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import { FieldValues, useForm } from 'react-hook-form';
+import Input from '../components/Input';
+import { Button } from 'flowbite-react';
+import { Auction } from '@/types';
+import { usePathname, useRouter } from 'next/navigation';
+import DateInput from '../components/DateInput';
+
+type Props = {
+    auction?: Auction
+}
+
+export default function AuctionForm({ auction }: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const { control, handleSubmit, setFocus, reset, formState: { isSubmitting, isValid} } = useForm({
+        mode: 'onTouched'
+    });
+
+    useEffect(() => {
+        if (auction) {
+            const { make, model, color, mileage, year } = auction;
+            reset({ make, model, color, mileage, year });
+        }
+        setFocus('make');
+    }, [setFocus, reset, auction])
+
+    async function onSubmit(data: FieldValues) {
+        console.log(data)
+    }
+    return (
+        <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
+            <Input label='Make' name='make' control={control}
+                rules={{ required: 'Make is required' }} />
+            <Input label='Model' name='model' control={control}
+                rules={{ required: 'Model is required' }} />
+            <Input label='Color' name='color' control={control}
+                rules={{ required: 'Color is required' }} />
+
+            <div className='grid grid-cols-2 gap-3'>
+                <Input label='Year' name='year' control={control} type='number'
+                    rules={{ required: 'Year is required' }} />
+                <Input label='Mileage' name='mileage' control={control} type='number'
+                    rules={{ required: 'Mileage is required' }} />
+            </div>
+
+            {pathname === '/auctions/create' &&
+            <>
+                <Input label='Image URL' name='imageUrl' control={control}
+                    rules={{ required: 'Image URL is required' }} />
+
+                <div className='grid grid-cols-2 gap-3'>
+                    <Input label='Reserve Price (enter 0 if no reserve)'
+                        name='reservePrice' control={control} type='number'
+                        rules={{ required: 'Reserve price is required' }} />
+                    <DateInput
+                        label='Auction end date/time'
+                        name='auctionEnd'
+                        control={control}
+                        dateFormat='dd MMMM yyyy h:mm a'
+                        showTimeSelect
+                        rules={{ required: 'Auction end date is required' }} />
+                </div>
+            </>}
+
+            <div className='flex justify-between'>
+                <Button outline color='gray'>Cancel</Button>
+                <Button
+                    isProcessing={isSubmitting}
+                    disabled={!isValid}
+                    type='submit'
+                    outline color='success'>Submit</Button>
+            </div>
+
+        </form>
+    )
+}
